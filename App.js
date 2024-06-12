0852b21f38ffd8e2833d7a10ad33b217ada89693
PurpleAliennn <elinekers@gmail.com>
2024-06-12 11:35:59 +0200

    styling for addplant

--- App.js
+++ App.js
@@ -8,15 +8,26 @@ import Dashboard from './dashboard';
 import Test from './test';
 import Test1 from './test1';
 import Test2 from './test2';
+import FeedScreen from './feedscreen';
+import FeedDetail from './feeddetail';
+import AddPlant from './addPlant';
 
 const Stack = createStackNavigator();
 
 const HomeScreen = ({ navigation }) => {
   return (
-
-    <View style = {styles.container} >
-      <Button title="Go to Dashboard" onPress={() => navigation.navigate('Dashboard')} />
-    <Dashboard></Dashboard>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Your App</Text>
+      <View style={styles.content}>
+        <View style={styles.horizontalLine} />
+        <View style={styles.buttonContainer}>
+          <Button title="Go to Dashboard" onPress={() => navigation.navigate('Dashboard')} />
+          <View style={styles.space} />
+          <Button title="Go to Feed" onPress={() => navigation.navigate('Feed')} />
+          <Button title="Add Plant" onPress={() => navigation.navigate('AddPlant')} />
+          <Button title="Ruilplanten" onPress={() => navigation.navigate('Feed', { userId })} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -28,6 +39,10 @@ const App = () => {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="FeedDetail" component={FeedDetail} />
+        <Stack.Screen name="AddPlant" component={AddPlant} />
+        <Stack.Screen name="Ruilplanten" component={FeedScreen} />
         <Stack.Screen name="test" component={Test} />
         <Stack.Screen name="test1" component={Test1} />
         <Stack.Screen name="test2" component={Test2} />
--- addPlant.js
+++ addPlant.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Alert, View, Text, Button, StyleSheet } from "react-native";
+import { Alert, View, Text, Button, StyleSheet, Pressable} from "react-native";
 import {TextInput} from "react-native-paper";
 import { getInstance, ref, set, get, push, child } from "firebase/database";
 import { initializeApp } from "firebase/app";
@@ -84,34 +84,106 @@ const NewPlant = ({ navigation }) => {
     // }, [clientToEdit]);
     
   return (
-    <View>
+
+    <View style={styles.main}>
+
+      <Text style={styles.text}> jouw plant </Text>
+      <Text style={styles.smallerText}> voeg hier de details toe van de plant die je in de ruil kas wilt zetten</Text>
+
       <TextInput
         label="Naam"
         value={plantName}
         onChangeText={setPlantName}
         placeholder="Naam"
+        style={styles.field}
+        mode="outlined"
+        outlineColor="#faf9f7"
+        activeOutlineColor="#1a1a1a"
       />
+
       <TextInput
         label="Afbeelding"
         value={image}
         onChangeText={setImage}
         placeholder="+"
+        style={styles.field}
+        mode="outlined"
+        outlineColor="#faf9f7"
+        activeOutlineColor="#1a1a1a"
       />
+
       <TextInput
         label="Locatie"
         value={location}
         onChangeText={setLocation}
         placeholder="Locatie"
+        style={styles.field}
+        mode="outlined"
+        outlineColor="#faf9f7"
+        activeOutlineColor="#1a1a1a"
       />
+
       <TextInput
         label="Omschrijving"
         value={description}
         onChangeText={setDescription}
         placeholder="Omschrijving"
+        style={styles.field}
+        mode="outlined"
+        outlineColor="#faf9f7"
+        activeOutlineColor="#1a1a1a"
       />
-      <Button title="Add Plant!" onPress={this.saveData} />
+
+      <Pressable onPress={this.saveData}>
+        <View  style={styles.button}>
+          <Text style={styles.buttonText}> toevoegen </Text>
+        </View>
+      </Pressable>
+
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#e07a5f',
+    height: '100%',
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+
+  field: {
+    backgroundColor: '#faf9f7',
+    width: 225,
+    marginBottom: 10,
+  },
+
+  button: {
+    backgroundColor: '#f0c6ba',
+    height: 50,
+    width: 150,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 15
+  },
+
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+
+  smallerText: {
+    textAlign: 'center',
+    width: '70%',
+    fontSize: 15,
+    marginBottom: 15
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 17
+  }
+})
 
 export default NewPlant;
