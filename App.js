89be69032a243a50cb23faa52b52fb4dff6c3938
Jaap Moerkerk <jaapiemoerkerk@hotmail.com>
2024-06-18 12:03:35 +0200

    Login and register with getAuth bug fixing after branch merging

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {StyleSheet, View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Dashboard from './dashboard';
 import Register from './Register';
 import Login from './Login';
@@ -22,33 +21,33 @@ const Stack = createStackNavigator();
 // const userId = auth.currentUser.uid;
 const userId = 1;
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to Your App</Text>
-      <View style={styles.buttonContainer}>
-        <Button className="account-btn" title="Registreren" onPress={() => navigation.navigate('Register')} />
-        <View style={styles.space} />
-        <Button className="account-btn" title="Login" onPress={() => navigation.navigate('Login')} />
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome to Your App</Text>
+            <View style={styles.buttonContainer}>
+                <Button className="account-btn" title="Registreren" onPress={() => navigation.navigate('Register')}/>
+                <View style={styles.space}/>
+                <Button className="account-btn" title="Login" onPress={() => navigation.navigate('Login')}/>
+            </View>
+            <View style={styles.content}>
+                <View style={styles.horizontalLine}/>
+                <View style={styles.buttonContainer}>
+                    <Button title="Go to Dashboard" onPress={() => navigation.navigate('Dashboard')}/>
+                    <View style={styles.space}/>
+                    <Button title="Go to Feed" onPress={() => navigation.navigate('Feed')}/>
+                </View>
+                <View style={styles.horizontalLine}/>
+                <View style={styles.buttonContainer}>
+                    <Button title="Add Plant" onPress={() => navigation.navigate('AddPlant')}/>
+                    <View style={styles.space}/>
+                    <Button title="Ruilplanten" onPress={() => navigation.navigate('Feed', {userId})}/>
+                    <View style={styles.space}/>
+                    <Button title="Go to KnnVerken" onPress={() => navigation.navigate('KnnVerken')}/>
+                </View>
+            </View>
         </View>
-      <View style={styles.content}>
-             <View style={styles.horizontalLine} />
-             <View style={styles.buttonContainer}>
-                    <Button title="Go to Dashboard" onPress={() => navigation.navigate('Dashboard')} />
-                     <View style={styles.space} />
-                     <Button title="Go to Feed" onPress={() => navigation.navigate('Feed')} />
-                     </View>
-                     <View style={styles.horizontalLine} />
-                     <View style={styles.buttonContainer}>
-                    <Button title="Add Plant" onPress={() => navigation.navigate('AddPlant')} />
-                    <View style={styles.space} />
-                   <Button title="Ruilplanten" onPress={() => navigation.navigate('Feed', { userId })} />
-                   <View style={styles.space} />
-                    <Button title="Go to KnnVerken" onPress={() => navigation.navigate('KnnVerken')} />
-                      </View>
-              </View>
-              </View>
-  );
+    );
 
 };
 
@@ -56,26 +55,26 @@ const HomeScreen = ({ navigation }) => {
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{animationEnabled:false}} initialRouteName="Home">
-                        <Stack.Screen name="Home" component={HomeScreen} />
-                        <Stack.Screen
-                          name="Dashboard"
-                          component={Dashboard}
-                          options={{
-                            headerLeft: () => null,
-                            title: 'Dashboard',
-                          }}
-                        />
-                <Stack.Screen name="Feed" component={FeedScreen} />
-                <Stack.Screen name="FeedDetail" component={FeedDetail} />
-                <Stack.Screen name="AddPlant" component={AddPlant} />
-                <Stack.Screen name="Ruilplanten" component={FeedScreen} />
-                <Stack.Screen name="ruilContact" component={RuilContact} />
-                <Stack.Screen name="KnnVerken" component={KnnVerken} />
-                <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ChatScreen" component={ChatScreen} />
-        <Stack.Screen name="Chat" component={Chat} />
+            <Stack.Navigator screenOptions={{animationEnabled: false}} initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen
+                    name="Dashboard"
+                    component={Dashboard}
+                    options={{
+                        headerLeft: () => null,
+                        title: 'Dashboard',
+                    }}
+                />
+                <Stack.Screen name="Feed" component={FeedScreen}/>
+                <Stack.Screen name="FeedDetail" component={FeedDetail}/>
+                <Stack.Screen name="AddPlant" component={AddPlant}/>
+                <Stack.Screen name="Ruilplanten" component={FeedScreen}/>
+                <Stack.Screen name="ruilContact" component={RuilContact}/>
+                <Stack.Screen name="KnnVerken" component={KnnVerken}/>
+                <Stack.Screen name="Register" component={Register}/>
+                <Stack.Screen name="Login" component={Login}/>
+                <Stack.Screen name="ChatScreen" component={ChatScreen}/>
+                <Stack.Screen name="Chat" component={Chat}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
